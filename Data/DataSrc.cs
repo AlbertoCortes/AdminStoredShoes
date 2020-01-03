edc63c72b4b782a70a4edbfd162780221fa08370
AlbertoCortes <alberto.cortesb@softtek.com>
2020-01-03 08:45:47 -0600

    First commit

---
+++ Data/DataSrc.cs
@@ -0,0 +1,35 @@
+﻿using Entities;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Data
+{
+   public class DataSrc
+    {
+        static DataProductsEntities model = new DataProductsEntities();
+
+        public static List<Productos> GetProductos()
+        {
+            var result = from mod in model.Products select mod ;
+            return SerializeJson<IEnumerable<Products>, List<Productos>>(result);
+        }
+
+        public static List<Imagen> GetImagen(int id)
+        {
+            var result = from mod in model.ImagesProduct where mod.IdImage == id select mod;
+            return SerializeJson<IEnumerable<ImagesProduct>, List<Imagen>>(result);
+        }
+
+
+        public static Out SerializeJson<In, Out>(In obj)
+        {
+            string output = JsonConvert.SerializeObject(obj);
+            return JsonConvert.DeserializeObject<Out>(output);
+        }
+
+
+        
+
+    }
+}
